e1f151de6dfb2700353571db54508c1e9af0a2d0
Alex Chernovsky <alexche8888@gmail.com>
2020-03-22 17:59:15 +0200

    add new form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,9 @@
 class GamesController < ApplicationController
   def new
-    @letters = (0..9).map { (65 + rand(26)).chr }
+    @letters = (0..9).map { (65 + rand(26)).chr }.join(" ")
   end
 
   def score
+
   end
 end
